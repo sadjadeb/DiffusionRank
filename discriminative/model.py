2b2585ee684fc4baefedcbfa406963661bbc0e5f
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2025-05-08 20:07:21 -0400

    Add discriminative classifier model

--- discriminative/model.py
+++ discriminative/model.py
@@ -28,3 +28,26 @@ class DNN(torch.nn.Module):
     def parameter_count(self):
         return sum(p.numel() for p in self.parameters() if p.requires_grad)
     
+
+
+class ClassifierDNN(torch.nn.Module):
+    def __init__(self, input_dim: int, num_hidden_layers: int = 3, num_hidden_nodes: int = 128, dropout_rate: float = 0.2, num_classes: int = 2):
+        super(ClassifierDNN, self).__init__()
+        layers = []
+        last_dim = input_dim
+        
+        for _ in range(num_hidden_layers):
+            layers.append(nn.Linear(last_dim, num_hidden_nodes))
+            layers.append(nn.ReLU())
+            layers.append(nn.LayerNorm(num_hidden_nodes))
+            layers.append(nn.Dropout(p=dropout_rate))
+            last_dim = num_hidden_nodes
+        layers.append(nn.Linear(last_dim, num_classes))
+        
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+    
+    def parameter_count(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
