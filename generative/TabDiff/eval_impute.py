3f134b4c932af284743fd4252978a8d626eb6262
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2025-05-27 23:22:58 -0400

    Update TabDiff evaluation file to report ndcg, precision, confusion_matrix, etc.

--- generative/TabDiff/eval_impute.py
+++ generative/TabDiff/eval_impute.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import f1_score, roc_auc_score
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score, precision_score, recall_score, confusion_matrix
 from sklearn.metrics import root_mean_squared_error
+from utils import calculate_metrics
 import argparse
 import json
 
@@ -13,6 +14,7 @@ parser.add_argument('--dataname', type=str, default='adult', help='Name of datas
 parser.add_argument('--exp_name', type=str, default=None)
 parser.add_argument('--col', type=int, default=0, help='Numerical Column to Impute')
 parser.add_argument('--non_learnable_schedule', action='store_true')
+parser.add_argument('--trial_size', type=int, default=50, help='Number of trials to average over')
 
 args = parser.parse_args()
 
@@ -44,7 +46,7 @@ if task_type == "binclass":
     real_y = encoder.fit_transform(real_target).toarray()
     
     syn_y = []
-    for i in range(50):
+    for i in range(args.trial_size):
         syn_path = f'impute/{dataname}/{exp_name}/{i}.csv'
         syn_data = pd.read_csv(syn_path)
         target = syn_data[target_col].to_numpy().reshape(-1, 1)
@@ -55,13 +57,57 @@ if task_type == "binclass":
     num_classes = np.max(syn_y_oh) + 1
     syn_y_oh = np.eye(num_classes)[syn_y_oh]
 
+    y_true = real_y.argmax(axis=1)
+    y_pred = syn_y_prob.argmax(axis=1)
+    y_pred_probs = syn_y_prob[:, 1]
     
+    
+    if info['name'] == 'MQ2007':
+        results = {}
+        for idx, label_t, label_p in zip(real_data['47'], y_true, y_pred_probs):
+            if idx not in results:
+                results[idx] = []
+            results[idx].append((label_t, label_p))
+        
+        avgndcg, avgp = calculate_metrics(results)
+        print(f"Avg NDCG: {avgndcg:.6f} - Avg Precision: {avgp:.6f}")
 
+    unique, counts = np.unique(y_pred, return_counts=True)
+    print("Predicted labels and their counts:")
+    for value, count in zip(unique, counts):
+        print(f"Value: {value}, Count: {count}")
+    print()
+
+    # Accuracy
+    accuracy = accuracy_score(y_true, y_pred)
+    print(f"Accuracy: {accuracy:.6f}")
+
+    # Precision (Macro and Micro Average)
+    precision_macro = precision_score(y_true, y_pred, average='macro')
+    precision_micro = precision_score(y_true, y_pred, average='micro')
+    print(f"Precision (Macro): {precision_macro:.6f} - Precision (Micro): {precision_micro:.6f}")
+
+    # Recall (Macro and Micro Average)
+    recall_macro = recall_score(y_true, y_pred, average='macro')
+    recall_micro = recall_score(y_true, y_pred, average='micro')
+    print(f"Recall (Macro): {recall_macro:.6f} - Recall (Micro): {recall_micro:.6f}")
+
+    # F1 Score (Macro and Micro Average)
+    f1_macro = f1_score(y_true, y_pred, average='macro')
+    f1_micro = f1_score(y_true, y_pred, average='micro')
+    print(f"F1 Score (Macro): {f1_macro:.6f} - F1 Score (Micro): {f1_micro:.6f}")
+
+    # Confusion Matrix
+    print("\nConfusion Matrix:")
+    print(confusion_matrix(y_true, y_pred))
+    print()
 
     micro_f1 = f1_score(real_y.argmax(axis=1), syn_y_prob.argmax(axis=1), average='micro')
+    print("Micro F1: ", round(micro_f1*100, 3))
+    macro_f1 = f1_score(real_y.argmax(axis=1), syn_y_prob.argmax(axis=1), average='macro')
+    print("Macro F1: ", round(macro_f1*100, 3))
     auc = roc_auc_score(real_y, syn_y_prob, average='micro')
-    auc_argmaxed = roc_auc_score(real_y, syn_y_oh, average='micro')
-    print("AUC: ", round(auc*100, 3))
+    print(f"AUC: {auc:.6f}")
 else:
     y_test = real_data[target_col].to_numpy()
     y_test = np.log(np.clip(y_test, 1, 20000))
