ae025cfba5f85a56a527d38e88fac2038fbd8365
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2026-01-30 01:13:24 -0500

    Make the paths in the EDA relative

--- EDA/features_distribution.py
+++ EDA/features_distribution.py
@@ -11,24 +11,23 @@ set_all_seeds(42)
 dataset_name = 'MQ2007'
 
 # Load the data
-project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if 'MQ200' in dataset_name or 'MSLR' in dataset_name:
-    data_dir = os.path.join(project_dir, 'data', dataset_name, 'npy', 'Fold1')
-else:
-    data_dir = os.path.join(project_dir, 'data', dataset_name)
-    
-X_num_train = np.load(os.path.join(data_dir, 'X_num_train.npy'))
-X_num_val = np.load(os.path.join(data_dir, 'X_num_val.npy'))
-X_num_test = np.load(os.path.join(data_dir, 'X_num_test.npy'))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+data_dir = os.path.join(project_root, 'data', dataset_name, 'by_fraction', 'Fold1', f'k{k}')
 
+
+X_train = np.load(os.path.join(data_dir, 'X_num_train.npy'))
 y_train = np.load(os.path.join(data_dir, 'y_train.npy'))
+
+X_val = np.load(os.path.join(data_dir, 'X_num_val.npy'))
 y_val = np.load(os.path.join(data_dir, 'y_val.npy'))
+
+X_test = np.load(os.path.join(data_dir, 'X_num_test.npy'))
 y_test = np.load(os.path.join(data_dir, 'y_test.npy'))
 
 # Concat features and labels of train, val, and test sets
-X_train = np.concatenate((y_train.reshape(-1, 1), X_num_train), axis=1)
-X_val = np.concatenate((y_val.reshape(-1, 1), X_num_val), axis=1)
-X_test = np.concatenate((y_test.reshape(-1, 1), X_num_test), axis=1)
+X_train = np.concatenate((y_train.reshape(-1, 1), X_train), axis=1)
+X_val = np.concatenate((y_val.reshape(-1, 1), X_val), axis=1)
+X_test = np.concatenate((y_test.reshape(-1, 1), X_test), axis=1)
 
 
 for y_index in trange(X_train.shape[1]):
